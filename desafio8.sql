dc4d7c472d0180b73e36e53ef985c96ce76916d8
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-31 14:48:33 -0300

    Feat: Requisito 09 Concluido

--- desafio8.sql
+++ desafio8.sql
@@ -1,3 +1,3 @@
-SELECT CONCAT(first_name, ' ', last_name) AS full_name,
-       CONCAT(city, '-', state_province, ', ', address) AS location
+SELECT CONCAT(first_name, ' ', last_name) AS 'full_name',
+       CONCAT(city, '-', state_province, ', ', address) AS 'location'
 FROM employees;
